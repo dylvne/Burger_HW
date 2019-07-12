1e6440c37fffeed94f2c1f6eb3b6172b5e19080a
Delane Chen <delane4.0@gmail.com>
2019-07-11 23:32:19 -0400

    so far I have been able to send information to the data base and then have it returned. currently I am trying to have the information sent from my burger js file to my handlebars files to then be displayed on the page. unfortunately I'm not sure if I'll be able to finish this without further instruction.

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,11 +1,57 @@
 $(document).ready(function(){
-    var input = $("#burger-input").val()
+    var list;
+    load()
+    
     
     $("button").on("click",function(){
+        var input = $("#burger-input").val()
         console.log(input)
+
         var newBurger = {
             name: input,
             devoured: false
         }
+
+        // Send the PUT request.
+    $.ajax("/api/burger", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function(data) {
+          console.log(data)
+          // Reload the page to get the updated list
+        location.reload();
+        }
+      );
     })
+
+    function load(){
+        $.ajax("/api/list").then(function(data){
+            console.log(data)
+            list = data
+
+            let plate = []
+                for(i=0;i<list.length;i++){
+                    if(list[i].devoured == false){
+                        plate.push(list[i])
+                    }
+                }
+
+            let tummy = []
+                for(i=0;i<list.length;i++){
+                    if(list[i].devoured == false){
+                        tummy.push(list[i])
+                    }
+                }
+
+                state = {
+                tummy: tummy,
+                plate: plate
+            }
+
+            module.exports = state;
+            
+        })
+    }
+    
 })
